c84b4f3dfb790420066668d0d70871a7e9e06beb
mhawley1230 <mhawley1230@gmail.com>
2017-05-18 12:09:46 -0400

    added first and last name to users, created model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_valid(:first_name).when('John', 'Sally') }
-  it { should_not have_valid(:first_name).when(nil, '') }
+  let(:user) { FactoryGirl.create(:user) }
 
-  it { should have_valid(:last_name).when('Smith', 'Swanson') }
-  it { should_not have_valid(:last_name).when(nil, '') }
-
-  it { should have_valid(:email).when('user@example.com', 'another@gmail.com') }
-  it { should_not have_valid(:email).when(nil, '', 'user',\
-    'users@com', 'usersba.com') }
-
-  # it { should have_valid(:avatar).when('https://profile.image.url.com/cookie.jpg')}
-
-  it 'has a matching password confirmation for the password' do
-    user = User.new
-    user.password = 'password'
-    user.password_confirmation = 'anotherpassword'
-
-    expect(user).to_not be_valid
-    expect(user.errors[:password_confirmation]).to_not be_blank
+  it "is valid with valid attributes" do
+    expect(user).to be_valid
   end
+
+  it "is not valid without a first name"
+
+  it "is not valid without a last name"
+
+  it "is not valid without a email"
+
+  it "is not valid without a password"
+
+  it "is not valid without a confirmation password"
+
+  # it { should have_valid(:first_name).when('John', 'Sally') }
+  # it { should_not have_valid(:first_name).when(nil, '') }
+  #
+  # it { should have_valid(:last_name).when('Smith', 'Swanson') }
+  # it { should_not have_valid(:last_name).when(nil, '') }
+  #
+  # it { should have_valid(:email).when('user@example.com', 'another@gmail.com') }
+  # it { should_not have_valid(:email).when(nil, '', 'user',\
+  #   'users@com', 'usersba.com') }
+  #
+  # # it { should have_valid(:avatar).when('https://profile.image.url.com/cookie.jpg')}
+  #
+  # it 'has a matching password confirmation for the password' do
+  #   user = User.new
+  #   user.password = 'password'
+  #   user.password_confirmation = 'anotherpassword'
+  #
+  #   expect(user).to_not be_valid
+  #   expect(user.errors[:password_confirmation]).to_not be_blank
+  # end
 end
