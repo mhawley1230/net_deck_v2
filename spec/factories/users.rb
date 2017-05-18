7f873f66edb21fb232718b13fa73115d355b0a83
mhawley1230 <mhawley1230@gmail.com>
2017-05-18 11:50:02 -0400

    user model test syntax changed, time to do research

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'John'
+    last_name 'Smith'
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password 'password'
   end
 end
