7251786d531e4869465490441e2fe8d0cf437ed7
mhawley1230 <mhawley1230@gmail.com>
2017-05-26 13:29:56 -0400

    installed jasmine-ajax

---
+++ spec/javascripts/support/ajax.js
@@ -0,0 +1,21 @@
+require('jasmine-ajax');
+
+// describe("mocking ajax", function() {
+//
+//   describe("suite wide usage", function() {
+//
+//     beforeEach(function() {
+//       jasmine.Ajax.install();
+//     });
+//
+//     afterEach(function() {
+//       jamine.Ajax.uninstall();
+//     });
+//
+//     describe("Async", function() {
+//       it("should fetch from a server", function() {
+//         var async = new Async();
+//       });
+//     });
+//   });
+// });
