289329802a01b3d6ef922681b445f2d36851ff04
Mike Hawley <mhawley1230@gmail.com>
2017-09-26 14:27:14 -0400

    swagger routes make me not want to die

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-      devise_for :users
-      root "tournaments#index"
 
-
-      resources :tournaments, only: [:index, :show] do
-        resources :decks, only: [:index, :show]
-      end
-
-      resources :decks, only: [:index, :show] do
-        resources :cards, only: [:index, :show]
-      end
-
-      resources :cards, only: [:show]
-    end
+  def add_swagger_route http_method, path, opts = {}
+    full_path = path.gsub(/{(.*?)}/, ':\1')
+    match full_path, to: "#{opts.fetch(:controller_name)}##{opts[:action_name]}", via: http_method
   end
+
+  add_swagger_route 'GET', '/api/v1//cards/findByColors', controller_name: 'cards', action_name: 'cards_find_by_colors_get'
+  add_swagger_route 'GET', '/api/v1//cards/findByName', controller_name: 'cards', action_name: 'cards_find_by_name_get'
+  add_swagger_route 'GET', '/api/v1//cards', controller_name: 'cards', action_name: 'index'
+  add_swagger_route 'GET', '/api/v1//cards/{id}', controller_name: 'cards', action_name: 'show'
+  add_swagger_route 'GET', '/api/v1//decks/findByName', controller_name: 'decks', action_name: 'decks_find_by_name_get'
+  add_swagger_route 'GET', '/api/v1//decks/findByPlacing', controller_name: 'decks', action_name: 'decks_find_by_placing_get'
+  add_swagger_route 'GET', '/api/v1//decks/findByPlayer', controller_name: 'decks', action_name: 'decks_find_by_player_get'
+  add_swagger_route 'GET', '/api/v1//decks', controller_name: 'decks', action_name: 'index'
+  add_swagger_route 'GET', '/api/v1//decks/{id}', controller_name: 'decks', action_name: 'show'
+  add_swagger_route 'GET', '/api/v1//tournaments/findByName', controller_name: 'tournaments', action_name: 'tournaments_find_by_name_get'
+  add_swagger_route 'GET', '/api/v1//tournaments', controller_name: 'tournaments', action_name: 'index'
+  add_swagger_route 'GET', '/api/v1//tournaments/{id}', controller_name: 'tournaments', action_name: 'show'
+  add_swagger_route 'POST', '/api/v1//tournaments', controller_name: 'tournaments', action_name: 'create'
 end
